fba5b1661d2aa2f94f28402b284c5748241c0da3
Daithi Coombes <webeire@gmail.com>
2016-12-21 18:04:37 +0000

    fix up code

--- index.js
+++ index.js
@@ -1,29 +1,28 @@
 "use strict";
 
 const config = {
-  iterate: 1000,
+    iterate: 1000,
+    arguments: [34,6298,234,2983,234,98,234],
 };
-const initArgs = [34,6298,234,2983,234,98,234];
 
 const fs = require('fs'),
-  bigoClass = require('./lib/bigo'),
+    bigoClass = require('./lib/bigo'),
     bigo = new bigoClass(config);
 
 bigo
-  .args(initArgs)
-  .run()
-  .then(runs => {
+    .run()
+    .then(runs => {
 
-    // model for d3
-    const d3Runs = runs.map((a, i)=>{
-      return {
-        size: a.args.length,
-        time: a.time.ms,
-      };
-    });
+        // model data for d3.js in front end.
+        const d3Runs = runs.map((a, i)=>{
+            return {
+                size: a.args.length,
+                time: a.time.ms,
+            };
+        });
 
-    fs.writeFile('data.json', JSON.stringify(d3Runs), e=>{
-      if(e) throw Error('Error writing to file: \n\t'+e.message);
-      console.log('Analysis complete, refresh webpage');
+        fs.writeFile('data.json', JSON.stringify(d3Runs), e=>{
+            if(e) throw Error('Error writing to file: \n\t'+e.message);
+            console.log('Analysis complete, refresh webpage');
+        });
     });
-  });
